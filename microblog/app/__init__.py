f8bcda11f401e71a531f2b26dd5ea18ed54253e9
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-11 21:14:32 +0200

    Create Database and Models Post model and User Model

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
     
 #  app name
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 # to avoid importing error we should import our app in the end
-from app import routes
+from app import routes,models
 
 
 
