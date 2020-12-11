b62b2ed810b095d08536941e301548828b2725e6
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-11 15:31:56 +0200

    1- Create a form and template form using flask_wtf module and wtforms module. 2- Rename run.py to microblog.py. 3- adding config file.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,8 +1,11 @@
-# Initialize application 
 from flask import Flask
+from app.config import Config
+    
 #  app name
 app = Flask(__name__)
+app.config.from_object(Config)
 # to avoid importing error we should import our app in the end
-from app import views
+from app import routes
+
 
 
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,8 @@
+from flask.helpers import url_for
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 
 # create our first view and give this view two paths the first path which is the main path is '/' the second path '/index'
@@ -18,3 +21,14 @@ def index():
         }
     ]
     return render_template('index.html',title='Home',user=user,posts=posts)
+
+
+# Create form view
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
