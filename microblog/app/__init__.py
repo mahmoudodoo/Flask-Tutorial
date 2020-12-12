eae14b862d1946daf6a4f554297574aca95817fa
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-12 14:57:12 +0200

    Create register form with it's view and template. Logic for User login and User logout.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
     
 #  app name
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 # to avoid importing error we should import our app in the end
 from app import routes,models
 
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,11 +3,26 @@ from app import app
 from flask import render_template
 from app.forms import LoginForm
 from flask import render_template, flash, redirect
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+
+from app import db
+from app.forms import RegistrationForm
+
+
+# Create Log out function 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 # create our first view and give this view two paths the first path which is the main path is '/' the second path '/index'
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'nickname': 'Miguel'}  # fake user
     posts = [  # fake array of posts
@@ -20,15 +35,37 @@ def index():
             'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template('index.html',title='Home',user=user,posts=posts)
+    return render_template('index.html',title='Home',posts=posts)
 
 
-# Create form view
-@app.route('/login',methods=['GET', 'POST'])
+# Create login form view
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+
+#  Create register form View
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
