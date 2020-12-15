3d6213c6ed0d4a0a5b5b16a7006333500da3f421
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-15 13:45:04 +0200

    Create a profile page and add an image to your profile Create Edit Profile Form Create Post Model and Template Add 2 fields to User Model "about_me" and "last_seen" Create Post templates and view Create edit_profile view and template

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -3,12 +3,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-    
+
 # Create User Model
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +17,9 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -25,7 +28,11 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
 # Create Post Model
 class Post(db.Model):
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -7,12 +7,14 @@ from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
-
+from datetime import datetime
 from app import db
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm,EditProfileForm
+from flask import request
 
 
-# Create Log out function 
+
+# Create Log out function
 @app.route('/logout')
 def logout():
     logout_user()
@@ -26,13 +28,13 @@ def logout():
 def index():
     user = {'nickname': 'Miguel'}  # fake user
     posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template('index.html',title='Home',posts=posts)
@@ -69,3 +71,39 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+
+# Create user view
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',form=form)
