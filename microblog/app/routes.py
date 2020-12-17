5d841ba47faad38f2ebd8407e6b96ec344959b28
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-17 08:22:41 +0200

    Create a custom Errors templates and views fo both 404, 500 codes Save all errors to logs send the error to the email Enable Debug mode

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -86,17 +86,18 @@ def user(username):
     return render_template('user.html', user=user, posts=posts)
 
 
+# before any request update and display the data time
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-
+# Edit Profile View
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
