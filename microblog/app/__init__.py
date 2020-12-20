76a4e6ea774160914dd0a20ee5df5d7f79aefef8
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-12-20 16:30:08 +0200

    Install flask bootstrab and use it in our Application install flask_moment and replace our date time with it

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 #  app name
 app = Flask(__name__)
@@ -13,6 +15,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # to avoid importing error we should import our app in the end
 from app import routes,models,errors
